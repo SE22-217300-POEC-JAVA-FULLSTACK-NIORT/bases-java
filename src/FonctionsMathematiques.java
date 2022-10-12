c14616ac8a8ff64e4971bede4e1fa16e4a6d4597
Christian Lisangola <jochri3@gmail.com>
2022-10-12 15:43:30 +0300

    Introduction des tests conditionnels

--- src/FonctionsMathematiques.java
+++ src/FonctionsMathematiques.java
@@ -1,5 +1,19 @@
 public class FonctionsMathematiques {
     public static void main(String[] args) {
+        double nombre=3;
+//        Puissance d'un nombre
+        double nombreAuCarre=Math.pow(nombre,2);
+        double nombreAuCube=Math.pow(nombre,3);
+        double nombreAALaPuissance10=Math.pow(nombre,10);
+        System.out.printf("3 au carré : %f\n",nombreAuCarre);
+        System.out.printf("3 au cube : %f\n",nombreAuCube);
+        System.out.printf("3 à la puissance 10 : %f\n",nombreAALaPuissance10);
 
+//        racine carré
+        double racineCarreNombre=Math.sqrt(9);
+        System.out.printf("Racine carré de 9 : %f",racineCarreNombre);
+
+//        PI
+        double pi=Math.PI;
     }
 }
